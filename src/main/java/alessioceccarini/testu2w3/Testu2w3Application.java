0117bea46d4dc3128129da9ee7c0d020647db4a2
AlessioCeccarini94 <alessioceccarini23@icloud.com>
2026-02-20 09:32:23 +0100

    firsr commit

---
+++ src/main/java/alessioceccarini/testu2w3/Testu2w3Application.java
@@ -0,0 +1,13 @@
+package alessioceccarini.testu2w3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Testu2w3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Testu2w3Application.class, args);
+	}
+
+}
---
+++ src/test/java/alessioceccarini/testu2w3/Testu2w3ApplicationTests.java
@@ -0,0 +1,13 @@
+package alessioceccarini.testu2w3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Testu2w3ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
